fd8a1dfde853334e742c4637fabfd2c763f24383
Daniel Romero <romerodaniel908@gmail.com>
2024-12-09 23:46:52 -0400

    feat: appointment confirmation logic implemented
    appointment confirmation logic implemented

--- websocketServer.js
+++ websocketServer.js
@@ -442,62 +442,104 @@ io.on('connection', (socket) => {
     const message = req.body.Body;
 
     try {
-      await prisma.$transaction(async (prisma) => {
-        const clientIdAndStatus = await prisma.clients.findFirst({
+      const clientIdStatusAppointments = await prisma.clients.findFirst({
+        where: {
+          mobile_phone: fromFormatted,
+        },
+        select: {
+          client_status_id: true,
+          id: true,
+          seller: {
+            select: {
+              id: true,
+            },
+          },
+          appointment: {
+            where: {
+              waiting_aprove: false,
+            },
+            select: {
+              id: true,
+              status_id: true,
+              start_date: true,
+            },
+          },
+        },
+      });
+
+      if (clientIdStatusAppointments && clientIdStatusAppointments.id) {
+        const data = await prisma.client_sms.create({
+          data: {
+            message: message,
+            date_sent: new Date(),
+            sent_by_user: false,
+            client_id: clientIdStatusAppointments.id,
+            status_id: 2,
+          },
+        });
+      }
+
+      if (
+        clientIdStatusAppointments &&
+        clientIdStatusAppointments.client_status_id &&
+        clientIdStatusAppointments.client_status_id === 1
+      ) {
+        const userStatus = await prisma.clients.update({
           where: {
             mobile_phone: fromFormatted,
           },
-          select: {
-            client_status_id: true,
-            id: true,
-            seller: {
-              select: {
-                id: true,
-              },
-            },
-          },
-        });
-
-        if (clientIdAndStatus && clientIdAndStatus.id) {
-          const data = await prisma.client_sms.create({
-            data: {
-              message: message,
-              date_sent: new Date(),
-              sent_by_user: false,
-              client_id: clientIdAndStatus.id,
-              status_id: 2,
-            },
-          });
-        }
-
-        if (
-          clientIdAndStatus &&
-          clientIdAndStatus.client_status_id &&
-          clientIdAndStatus.client_status_id === 1
-        ) {
-          const userStatus = await prisma.clients.update({
-            where: {
-              mobile_phone: fromFormatted,
-            },
-            data: {
-              client_status_id: 2,
-            },
-          });
-        }
-
-        // create a new lead register
-
-        const lead = await prisma.client_has_lead.create({
           data: {
-            created_at: new Date(),
-            assigned_to_id: clientIdAndStatus.seller.id || 1,
-            client_id: clientIdAndStatus.id,
-            status_id: 2,
-            created_by_id: clientIdAndStatus.seller.id || 1,
-            lead_id: 7,
+            client_status_id: 2,
           },
         });
+      }
+
+      // create a new lead register
+
+      const lead = await prisma.client_has_lead.create({
+        data: {
+          created_at: new Date(),
+          assigned_to_id: clientIdStatusAppointments.seller.id || 1,
+          client_id: clientIdStatusAppointments.id,
+          status_id: 2,
+          created_by_id: clientIdStatusAppointments.seller.id || 1,
+          lead_id: 7,
+        },
       });
+
+      // check if the message contain 'Y' , 'N' or 'S'
+
+      const specialCharacters = ['Y', 'N', 'S'];
+
+      const messageSplitted = message.split(' ');
+
+      if (messageSplitted.every((word) => specialCharacters.includes(word))) {
+        // check if the customer has a pending for confirmation appointment
+
+        if (clientIdStatusAppointments.appointment) {
+          clientIdStatusAppointments.appointment.forEach(async (el) => {
+            if (el.status_id === 1 && new Date(el.start_date) > new Date()) {
+              await prisma.appointments.update({
+                where: {
+                  id: el.id,
+                },
+                data: {
+                  appointments_status: 6,
+                },
+              });
+            }
+          });
+
+          await prisma.clients.update({
+            where: {
+              id: clientIdStatusAppointments.id,
+            },
+            data: {
+              client_status_id: 6,
+            },
+          });
+        }
+      }
 
       await prisma.$disconnect();
 
