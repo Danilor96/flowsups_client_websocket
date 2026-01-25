2c445c6cb629cb12d11c296e5afec891a071d148
Daniel R. <58092368+Danilor96@users.noreply.github.com>
2026-01-25 05:15:22 -0400

    feat: logic for handling incoming phone formats implemented
    New block for handling incoming data added. Prisma unique constraint error handling for generate notes, leads, notifications & updates some customer data

--- src/libs/incomingLeads/incomingLeads.ts
+++ src/libs/incomingLeads/incomingLeads.ts
@@ -1,12 +1,20 @@
+import { Prisma } from '@prisma/client';
 import { io } from '../../websocketServer';
 import { prisma } from '../prisma/prisma';
 import { ADFData } from './types';
 
 export async function incomingLeads(adfData: ADFData) {
+  let countryCodeId: number | null = null;
+  let phoneNumber = '';
+  let customerEmail = '';
+  let providerName = '';
+
   try {
     const { customer, provider, requestdate, vehicle, vendor } = adfData.prospect;
 
     const { email, name, phone } = customer.contact;
+
+    customerEmail = email;
 
     const [first, last] = name;
 
@@ -18,12 +26,12 @@ export async function incomingLeads(adfData: ADFData) {
       },
     });
 
-    const providerName = provider?.name;
+    providerName = provider?.name;
     let existsLeadSource;
-    if(providerName && providerName.length > 0) {
+    if (providerName && providerName.length > 0) {
       existsLeadSource = await prisma.lead_sources.findFirst({
         where: {
-          source: providerName.trim(),
+          source: providerName.toLowerCase().trim(),
         },
       });
 
@@ -41,11 +49,30 @@ export async function incomingLeads(adfData: ADFData) {
       providerName,
     });
 
+    const onlyNumbersCharacters = phone?.replace(/\D/g, '');
+    phoneNumber = onlyNumbersCharacters.slice(-10);
+    const areaCode = onlyNumbersCharacters.slice(0, -10);
+
+    if (areaCode) {
+      const countryPhoneCode = await prisma.country_phone_code.upsert({
+        where: {
+          code: `+${areaCode}`,
+        },
+        update: {},
+        create: {
+          code: `+${areaCode}`,
+        },
+      });
+
+      countryCodeId = countryPhoneCode.id;
+    }
+
     const newUser = await prisma.clients.create({
       data: {
         current_address: '',
         email: email || null,
-        mobile_phone: phone?.replaceAll(' ', '') || null,
+        mobile_phone: phoneNumber || null,
+        country_phone_code_id: countryCodeId,
         first_name: first._ || '',
         last_name: last._ || '',
         name_lastname: `${first._ || ''} ${last._ || ''}`,
@@ -86,14 +113,116 @@ export async function incomingLeads(adfData: ADFData) {
     console.log('Guardado');
   } catch (error) {
     console.log(error);
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        if (countryCodeId) {
+          await prisma.clients.update({
+            where: {
+              mobile_phone: phoneNumber,
+            },
+            data: {
+              country_phone_code_id: countryCodeId,
+            },
+          });
+        }
+
+        const mssg = `The system detected the customer as a duplicate due to repeat entry today${providerName ? ` from ${providerName}.` : '.'}`;
+
+        const note = await prisma.notes.create({
+          data: {
+            note: mssg,
+            created_at: new Date(),
+            client_note: {
+              connect: {
+                mobile_phone: phoneNumber,
+              },
+            },
+          },
+          select: {
+            id: true,
+            client_id: true,
+            client_note: {
+              select: {
+                seller_id: true,
+                bdc_id: true,
+              },
+            },
+          },
+        });
+
+        await prisma.client_has_lead.create({
+          data: {
+            created_at: new Date(),
+            status_id: 2,
+            client_id: note.client_id,
+            note_id: note.id,
+            lead_id: 1,
+          },
+        });
+
+        await prisma.notifications.create({
+          data: {
+            message: mssg,
+            type_id: 4,
+            user_id: note.client_note.seller_id,
+            notification_for_managers: true,
+            customer_id: note.client_id,
+          },
+        });
+      }
+    }
   }
 }
 
-export const filterNumber = (value: string) => {
-  let valueFiltered = value?.replace(/[^-\d.]/g, '');
-
-  return valueFiltered;
+const adfTest: ADFData = {
+  prospect: {
+    provider: { name: 'Facebook K Bots', service: '[]', url: '' },
+    requestdate: '2025-09-24T17:57:00',
+    vehicle: {
+      year: '2022',
+      make: 'Toyota',
+      model: 'Corolla',
+      odometer: {
+        $: {
+          units: '1',
+        },
+        _: '',
+      },
+      $: {
+        interest: '',
+        status: '',
+      },
+      trim: '',
+    },
+    customer: {
+      comments: '',
+      contact: {
+        name: [
+          {
+            _: 'Dan',
+            $: {
+              part: 'first',
+            },
+          },
+          {
+            _: 'France',
+            $: {
+              part: 'last',
+            },
+          },
+        ],
+        email: '',
+        phone: '+1 (321) 123-4567',
+      },
+    },
+    vendor: {
+      vendorname: 'FlowsUps Motors',
+    },
+  },
 };
+
+incomingLeads(adfTest);
 
 // {
 //   adf: {
